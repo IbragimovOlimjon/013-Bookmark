923f1d31d8b846e05c1ff9c4b5fcab1c26e2cb08
IbragimovOlimjon <ibragimovolimjon339@gmail.com>
2022-05-26 14:31:27 +0300

    the features section of the site and the acardion section have been added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// let acc = document.getElementsByClassName("frequently-questions__acardion");
+// let i;
+
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+        
+//         let frequentlyQuestions__panel = this.nextElementSibling;
+//         if (frequentlyQuestions__panel.style.display === "block") {
+//             frequentlyQuestions__panel.style.display = "none";
+//         } else {
+//             frequentlyQuestions__panel.style.display = "block";
+//         }
+        
+//     });
+// }
+
+// array.forEach(element => {
+
+// });
