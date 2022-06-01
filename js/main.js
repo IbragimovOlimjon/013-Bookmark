9bee55270c8db444ee380ab714fa25b156c5dab4
IbragimovOlimjon <ibragimovolimjon339@gmail.com>
2022-06-01 18:02:19 +0300

    add changes to the site

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const elsFeaturesItem = document.querySelectorAll('.features__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
-
+const elsAccordionItem = document.querySelectorAll('.accordion__item');
 
 elsFeaturesItem.forEach(function (item, index){
     elsFeaturesItem[index].addEventListener('click', function(){
@@ -10,4 +10,14 @@ elsFeaturesItem.forEach(function (item, index){
         elsFeaturesItem[index].classList.add('features__item--active');
     });
 
+});
+
+// ACCORDION//
+elsAccordionItem.forEach(function (item, index){
+    item.addEventListener('click', function(){
+        elsAccordionItem.forEach(function(){
+            elsAccordionItem[index].classList.toggle("accordion__item--open")
+        })
+        elsAccordionItem[index].classList.toggle('accordion__item--open');
+    });
 });
